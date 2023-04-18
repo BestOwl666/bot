2719e5efc0538b0f8e0cf23fcec19157347a00f3
BestOwl666 <131203430+BestOwl666@users.noreply.github.com>
2023-04-18 23:14:47 +0300

    Add files via upload

--- Botick.py
+++ Botick.py
@@ -6,15 +6,18 @@ from telebot import types
 import pyautogui
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import subprocess
+from dotenv import load_dotenv
 
+load_dotenv()
 
-bot = telebot.TeleBot('6075909461:AAE_lmqFiTuW_arXv6VJbWPZ8yUhDRYKf-s')
+PROXY_URL = 
+bot = Bot(token=os.environ.get('TOKEN'), proxy=PROXY_URL)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton('GTA V', callback_data='button1')                   #ЕСТЬ
-    button2 = types.InlineKeyboardButton('ЧИТ', callback_data='button2')                     #ЕСТЬ
+    button2 = types.InlineKeyboardButton('Скрипт', callback_data='button2')                     #ЕСТЬ
     button3 = types.InlineKeyboardButton('Скриншот', callback_data='screenshot')             #ЕСТЬ
     button4 = types.InlineKeyboardButton('Заблокировать экран', callback_data='button4')     #ЕСТЬ
     button5 = types.InlineKeyboardButton('Перезагрузка', callback_data='button5')            #ЕСТЬ
@@ -35,7 +38,7 @@ def callback_query(call):
 
                                                                                 #Запустить ЧИТ!
     if call.data == 'button2':
-        bot.answer_callback_query(call.id, 'Запуск чита')
+        bot.answer_callback_query(call.id, 'Запуск скрипта')
         pass
         file_path = 'F:\GTA Online/555v7fVFkTzq4JuUmq'
         os.startfile(file_path)
